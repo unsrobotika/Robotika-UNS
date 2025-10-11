9df389a5c7da2c6813afa2ea04a5a9708d09656d
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-10-11 20:16:23 +0700

    create atom style

---
+++ src/components/atom/buttom.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Buttom({text}){
+    return(
+        <div>
+            <p>{text}</p>
+        </div>
+    )
+}
---
+++ src/components/organism/header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header(){
+    return(
+        <div className="text-white text-5xl w-full ">
+            <h1 className="text-center">ROBOTIKA UNS</h1>
+        </div>
+    )
+}
