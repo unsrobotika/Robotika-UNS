43d625120c17ac11d4d528b6c4330d0ad9a9ea57
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-10-24 21:45:51 +0700

    add UNS logo

--- src/components/organism/header.jsx
+++ src/components/organism/header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <div>
             <div className="flex text-gray-300 p-2 text-xs md:text-lg bg-black ">
                 <div className="flex-1/3"></div>
-                <div className=""><Zap className='inline'/>Robotika <span className='text-blue-400'> Universitas Sebelas Maret </span></div>
+                <div className="mr-9"><Zap className='inline'/>Robotika <span className='text-blue-400'> Universitas Sebelas Maret </span></div>
 
             </div>
             <Nav />
@@ -18,17 +18,19 @@ export default function Header(){
 function Nav(){
     return (
         <div className='flex text-gray-300 bg-black items-center'>
-            <div className=' flex justify-center w-1/4'>
+            <div className=' flex justify-center w-1/6'>
                     <img src='./MainLogo.jpg' className='h-14 w-auto my-1'/>
             </div>
-            <nav className='flex self-stretch w-3/4'>
+            <div className='w-1/6 flex items-center-safe'>
+                <img src='https://uns.ac.id/en/wp-content/uploads/2025/03/logo-uns-2-768x289.webp' className='h-10 w-auto my-1'/>
+            </div>
+            <nav className='flex self-stretch w-4/6'>
                 <a className=' flex items-center hover:bg-gray-900 grow justify-center'>Home</a>
                 <a className=' flex items-center hover:bg-gray-900 grow justify-center'>Siapa Kita</a>
                 <a className=' flex items-center hover:bg-gray-900 grow justify-center'>Apa yang Kita Lakukan</a>
                 <a className=' flex items-center hover:bg-gray-900 grow justify-center'>Pencapaian</a>
                 <a className=' flex items-center hover:bg-gray-900 grow justify-center'>Daftar</a>
                 
-               
             </nav>
             
                 
---
+++ src/components/organism/teamName.jsx
@@ -0,0 +1,10 @@
+
+
+export default function TeamName(){
+    return (
+        <div>
+            <div>
+                  </div>
+        </div>
+    )
+}
